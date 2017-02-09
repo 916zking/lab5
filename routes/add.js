f01f473ea9b4e36b8618adcc422a555f65066708
Zackary King <zdking@ucsd.edu>
2017-02-08 18:35:02 -0800

    Added all the things

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,18 @@
 var data = require("../data.json");
 
 exports.addFriend = function(req, res) {    
-	// Your code goes here
+	console.log("It worked");
+	var name = req.query.name;
+	var description = req.query.description;
+	//var newFriend = '"name":'+name+', "description":'+description+', "imageURL: "lorempixel.com/400/400/people";';
+	var newFriend = {
+			"name": name,
+			"description": description,
+			"imageURL": "http://lorempixel.com/400/400/people"			
+		};
+	data.friends.push(newFriend);
+	console.log(newFriend);
+	console.log(data);
+	res.render('index.handlebars')
+
  }
